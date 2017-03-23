8db6fd08a09ebabe80b8404b5c9eaed2ec20860d
p0o <pooria_az@ymail.com>
2017-03-23 11:39:04 +0800

    make the tinder-like swiping page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,50 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, Image, Text, View, StyleSheet } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import SwipeCards from 'react-native-swipe-cards';
 
-class HomeScreen extends Component {
+// temporarily hardcoded
+const dogPhoto1 = 'https://i.redditmedia.com/t1KLBI5M8dhEvR0rNjDaOrHNLIrSxomY0Kp29zjeijc.jpg?s=5d60f4a8695f13ca2a0d7da4c008c288';
+const dogPhoto2 = 'https://i.redditmedia.com/Bi9zaToQzQurkI7SvweFYiasebXnifkch2tNSNVxp10.jpg?fit=crop&amp;crop=faces%2Centropy&amp;arh=2&amp;w=960&amp;s=8828ef2382bf8d775263f86a6b3b2ec4';
+
+const data = [dogPhoto1, dogPhoto2];
+
+const styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 300,
+    height: 300,
+  },
+});
+
+export const PhotoCard = ({ imageUri }) => (
+  <View style={styles.wrapper}>
+    <Image
+      style={styles.image}
+      source={{ uri: imageUri }}
+    />
+  </View>
+);
+
+export class HomeScreen extends Component {
   static navigationOptions = {
     title: 'React Native Aww',
   };
 
   render() {
-    return <Text>Hello, Navigation!</Text>;
+    return (
+      <View style={styles.wrapper}>
+        <SwipeCards
+          cards={data}
+          renderCard={(imageUri) => <PhotoCard imageUri={imageUri} />}
+        />
+      </View>
+    );
   }
 }
 
