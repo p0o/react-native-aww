72c69bdfdb7986484ff1859be8d682adbf7b940d
p0o <pooria_az@ymail.com>
2017-03-23 11:40:26 +0800

    add tests for HomeScreen and PhotoCard

---
+++ src/App.test.js
@@ -0,0 +1,34 @@
+import 'react-native';
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+// requirements
+import { StackNavigator } from 'react-navigation';
+import SwipeCards from 'react-native-swipe-cards';
+
+// test components
+import { HomeScreen, PhotoCard } from '../src/App.js';
+
+test('should render HomeScreen correctly', () => {
+  const tree = renderer.create(
+    <HomeScreen />
+  ).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+test('should render PhotoCard correctly', () => {
+  const tree = renderer.create(
+    <PhotoCard />
+  ).toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
+
+test('should render PhotoCard with the imageUri props', () => {
+  const mockImageUri = 'http://someUrl/photo.jpg';
+  const tree = renderer.create(
+    <PhotoCard imageUri={mockImageUri} />
+  ).toJSON();
+  const imageComponent = tree.children[0];
+  expect(imageComponent.props.source).toEqual({ uri: mockImageUri });
+});
