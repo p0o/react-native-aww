dbbc855ad17bfa5ad3bee5efcb3306cd3151b7c3
p0o <pooria_az@ymail.com>
2017-03-23 15:33:13 +0800

    add redux-logger for debugging

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { AppRegistry } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 import reducer from './reducers';
 import HomeScreen from './HomeScreen/HomeScreen';
@@ -11,7 +12,11 @@ const AwwAppNavigator = StackNavigator({
   Home: { screen: HomeScreen },
 });
 
-const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
+const logger = createLogger({
+  duration: true,
+});
+
+const createStoreWithMiddleware = applyMiddleware(logger, thunk)(createStore);
 const store = createStoreWithMiddleware(reducer);
 
 export default class AwwApp extends Component {
